f8799841e46f75900d5ea9a58316bd1aedf04e23
omarlonRosa <marlonrosapl@gmail.com>
2026-01-14 10:26:41 -0300

    Fix: config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,15 +1,13 @@
+const isDevelopment = import.meta.env.DEV;
 
-
-export const API_BASE_URL = "http://192.168.1.82:8030";
-
+export const API_BASE_URL = isDevelopment 
+    ? "http://localhost:8000"          
+    : "http://192.168.1.82:8030";   
 
 export const getMediaUrl = (path: string) => {
     if (!path) return '';
-
     if (path.startsWith('http')) return path;
-
-
+    
     const cleanPath = path.startsWith('/') ? path.substring(1) : path;
-
     return `${API_BASE_URL}/${cleanPath}`;
 };
