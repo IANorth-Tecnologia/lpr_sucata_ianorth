e91c07bbd81373fb00e6729acf3e3fbb5ca18f5f
omarlonRosa <marlonrosapl@gmail.com>
2026-02-27 11:49:16 -0300

    fix: corrige conexão com banco de dados

--- backend/src/database.py
+++ backend/src/database.py
@@ -13,7 +13,13 @@ if not SQLALCHEMY_DATABASE_URL:
     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 else:
     print(f"Conectando ao Banco de Produção...")
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        pool_size=20,
+        max_overflow=40,
+        pool_pre_ping=True,
+        pool_recycle=1800
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
